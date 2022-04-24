98bb1b744fcb24a41fdce06196e056e9b41ce870
alswnsp411 <alswnsp411@naver.com>
2022-04-24 11:51:47 +0900

    세 개 이상의 화면 만들어 전환하기

--- DoitMission08/app/src/main/java/com/example/doitmission_08/CustomActivity.java
+++ DoitMission08/app/src/main/java/com/example/doitmission_08/CustomActivity.java
@@ -2,13 +2,39 @@ package com.example.doitmission_08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CustomActivity extends AppCompatActivity {
+    Button cmenuButton;
+    Button cloginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom);
+
+        cmenuButton = findViewById(R.id.cmenuButton);
+        cloginButton = findViewById(R.id.cloginButton);
+
+        cmenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- DoitMission08/app/src/main/java/com/example/doitmission_08/LoginActivity.java
+++ DoitMission08/app/src/main/java/com/example/doitmission_08/LoginActivity.java
@@ -2,13 +2,42 @@ package com.example.doitmission_08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText id;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        id = findViewById(R.id.idEditText);
+        password = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 비어있는지 확인 (equals)
+                if(id.getText().toString().equals("") || password.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"아이디와 비밀번호를 입력해주세요.",Toast.LENGTH_SHORT).show();
+                }
+                else{  // 메뉴액티비티에 아이디와 비밀번호를 전달
+                    Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+                    intent.putExtra("id",id.getText().toString());
+                    intent.putExtra("password",password.getText().toString());
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
     }
 }
